5b91815a2f10c91c64f7d92b4b66b8e4cd1d0792
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-09-10 19:01:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# lita-notification_weather
+
+Slack suppoly
+Notification of Daito weather
+
+
+## Installation
+
+Add lita-notification_weather to your Lita instance's Gemfile:
+json
+open-uri
+
+
+``` ruby
+gem "lita-notification_weather" 
+```
+
+## Configuration
+
+TODO: Describe any configuration attributes the plugin exposes.
+
+## Usage
+
+***COMAND***
+w_now
+=> Notification of Daito weather
---
+++ lib/lita/handlers/notification_weather.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+module Lita
+  module Handlers
+    class NotificationWeather < Handler
+      route /^w_now/, :get_Weather, help:{"w_now" => "Rain notification"}
+
+      def get_Weather(response)
+        response.reply "大阪府大東市"
+        uri = 'http://api.openweathermap.org/data/2.5/weather?q=daito,jp'
+        result = JSON.load(open(uri).read)
+        weather_now = result["weather"][0]["main"]
+        response.reply "現在の天気 #{weather_now}"
+        weather_now_desc = result["weather"][0]["description"]
+        response.reply "詳細 #{weather_now_desc}"
+        humidity_now = result["main"]["humidity"]
+        response.reply "湿気 #{humidity_now}%"
+      end
+    end
+      Lita.register_handler(self)
+  end
+end
